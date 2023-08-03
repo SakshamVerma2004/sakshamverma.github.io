2698cc54bdf89fb31ec50a55838fa222eecb9c87
SakshamVerma2004 <reemaverma001@gmail.com>
2023-08-03 17:42:53 +0530

    finally added

--- my_portfolio/src/Components/Resume.jsx
+++ my_portfolio/src/Components/Resume.jsx
@@ -2,7 +2,7 @@ import styles from "./Resume.module.css";
 let Resume=()=>{
     return (
         <div className={styles.main}>
-            <a className={styles.heading} href="https://drive.google.com/file/d/19ddtCOit2wEi5aG8o7redxHWp7RATiSC/view?usp=sharing" target="_blank">View Full Resume</a>
+            <a className={styles.heading} href="https://drive.google.com/file/d/1z6Ii2_EndhltsKhnP4WFH0-o-CqvRubR/view?usp=sharing" target="_blank">View Full Resume</a>
         </div>
     )
 }
